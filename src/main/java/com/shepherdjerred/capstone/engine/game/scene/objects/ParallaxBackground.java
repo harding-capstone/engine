978ddf8c02bcd726f902ecf0e42ac056ea2a801b
Jerred Shepherd <ShepherdJerred@gmail.com>
2019-04-09 20:08:51 -0500

    Work more on parallax backgrounds

--- src/main/java/com/shepherdjerred/capstone/engine/game/scene/objects/ParallaxBackground.java
+++ src/main/java/com/shepherdjerred/capstone/engine/game/scene/objects/ParallaxBackground.java
@@ -3,7 +3,8 @@ package com.shepherdjerred.capstone.engine.game.scene.objects;
 import com.shepherdjerred.capstone.engine.engine.scene.GameObject;
 import com.shepherdjerred.capstone.engine.engine.scene.GameObjectRenderer;
 import com.shepherdjerred.capstone.engine.engine.scene.position.ScenePosition;
-import lombok.AllArgsConstructor;
+import java.util.SortedMap;
+import java.util.TreeMap;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -13,13 +14,30 @@ import lombok.ToString;
  */
 @Getter
 @ToString
-@AllArgsConstructor
-public class ParallaxBackground implements GameObject {
+public class ParallaxScrollingBackground implements GameObject {
 
-  private final GameObjectRenderer<ParallaxBackground> renderer;
+  private final GameObjectRenderer<ParallaxScrollingBackground> renderer;
   @Setter
   private ScenePosition position;
   private final Type type;
+  private SortedMap<Integer, SortedMap<Integer, ScenePosition>> positions;
+
+  public ParallaxScrollingBackground(GameObjectRenderer<ParallaxScrollingBackground> renderer,
+      ScenePosition scenePosition,
+      Type type) {
+    this.renderer = renderer;
+    this.position = scenePosition;
+    this.type = type;
+    this.positions = new TreeMap<>();
+
+    positions.put(1, new TreeMap<>());
+    positions.put(2, new TreeMap<>());
+    if (type == Type.PURPLE_MOUNTAINS) {
+      positions.forEach((instance, layerMap) -> {
+
+      });
+    }
+  }
 
   public enum Type {
     CEMETERY,
